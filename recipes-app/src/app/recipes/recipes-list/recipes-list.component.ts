b639eceba23ae16b241f0479a445a64921b4cac5
Salvabb81 <salvabb81@users.noreply.github.com>
2020-12-04 18:41:37 +0100

    Adding models and working on components

--- recipes-app/src/app/recipes/recipes-list/recipes-list.component.ts
+++ recipes-app/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This  is simply a test', 
+    'https://p0.pikist.com/photos/666/25/food-meat-recipe-power-pork-dishes.jpg'),
+    new Recipe('A Test Recipe', 'This  is simply a test', 
+    'https://p0.pikist.com/photos/666/25/food-meat-recipe-power-pork-dishes.jpg')
+  ];
   constructor() { }
 
   ngOnInit(): void {
