b2495cd08cc964afaa2aa11f42690cd5550002aa
Salvabb81 <salvabb81@users.noreply.github.com>
2020-12-29 10:47:32 +0100

    Transforming Response Data

--- recipes-app/src/app/recipes/recipe.service.ts
+++ recipes-app/src/app/recipes/recipe.service.ts
@@ -8,24 +8,25 @@ import { Recipe } from "./recipe.model";
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe(
-            'A Test Recipe', 
-            'This  is simply a test', 
-            'https://p0.pikist.com/photos/666/25/food-meat-recipe-power-pork-dishes.jpg',
-            [
-                new Ingredient('Meat', 1),
-                new Ingredient('French fries', 20)
-            ]),
-        new Recipe(
-            'Another Test Recipe', 
-            'This  is simply a test', 
-            'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872',
-            [
-                new Ingredient('Buns', 2),
-                new Ingredient('Meat', 1)
-            ])
-      ];
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         'A Test Recipe', 
+    //         'This  is simply a test', 
+    //         'https://p0.pikist.com/photos/666/25/food-meat-recipe-power-pork-dishes.jpg',
+    //         [
+    //             new Ingredient('Meat', 1),
+    //             new Ingredient('French fries', 20)
+    //         ]),
+    //     new Recipe(
+    //         'Another Test Recipe', 
+    //         'This  is simply a test', 
+    //         'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872',
+    //         [
+    //             new Ingredient('Buns', 2),
+    //             new Ingredient('Meat', 1)
+    //         ])
+    //   ];
+    private recipes: Recipe[] = [];
 
     constructor(private slService: ShoppingListService) {
 
