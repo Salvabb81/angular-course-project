30a5c02714437d5c8dc71181f7e3df390b958f4b
Salvabb81 <salvabb81@users.noreply.github.com>
2021-01-05 09:22:09 +0100

    Using Environment Variables

--- recipes-app/src/app/auth/auth.service.ts
+++ recipes-app/src/app/auth/auth.service.ts
@@ -5,6 +5,8 @@ import { BehaviorSubject, Subject, throwError } from "rxjs";
 import { User } from "./user.model";
 import { Router } from "@angular/router";
 
+import { environment } from '../../environments/environment';
+
 export interface AuthResponseData {
     kind: string;
     idToken: string;
@@ -23,7 +25,7 @@ export class AuthService {
     constructor(private http: HttpClient, private router: Router) {}
 
     signup(email: string, password: string) {
-        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAdikcM7TPyNYqTXMnflYXEPI0famO4GFo',
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + environment.firebaseAPIKey,
         {
             email: email,
             password: password,
@@ -36,7 +38,7 @@ export class AuthService {
 
     login(email: string, password: string) {
         return this.http.post<AuthResponseData>(
-            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAdikcM7TPyNYqTXMnflYXEPI0famO4GFo',
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + environment.firebaseAPIKey,
         {
             email: email,
             password: password,
